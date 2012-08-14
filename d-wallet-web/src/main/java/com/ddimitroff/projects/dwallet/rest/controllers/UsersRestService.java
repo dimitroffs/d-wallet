4d25b00b848624dd68415e0ef3f09b570caf0211
ddimitroff <mykob.11@gmail.com>
2012-08-15 01:11:02 +0300

    register new user implemented for android device few configurations updated

--- d-wallet-web/src/main/java/com/ddimitroff/projects/dwallet/rest/controllers/UsersRestService.java
+++ d-wallet-web/src/main/java/com/ddimitroff/projects/dwallet/rest/controllers/UsersRestService.java
@@ -6,6 +6,7 @@ import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -71,6 +72,7 @@ public class UsersRestService {
 	// DWallet-API-Key: <api-key> needed
 	@RequestMapping(method = RequestMethod.POST, value = "/users/register")
 	@ResponseStatus(value = HttpStatus.OK)
+	@Transactional
 	public void registerUser(
 			@RequestHeader(value = DWalletRestUtils.DWALLET_REQUEST_HEADER, required = false) String apiKey,
 			@RequestBody UserRO userRO) throws DWalletResponseException {
