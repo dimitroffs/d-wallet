02d60f26134dfc80986388c2759e1d16c6cb87a6
ddimitroff <mykob.11@gmail.com>
2012-09-05 00:47:51 +0300

    Cash enums modified

--- d-wallet-web/src/main/java/com/ddimitroff/projects/dwallet/enums/CashFlowCurrencyType.java
+++ d-wallet-web/src/main/java/com/ddimitroff/projects/dwallet/enums/CashFlowCurrencyType.java
@@ -1,28 +1,26 @@
 package com.ddimitroff.projects.dwallet.enums;
 
 public enum CashFlowCurrencyType {
-	BGN(1), USD(2), EUR(3);
+  BGN(1), USD(2), EUR(3);
 
-	private final int currencyCode;
+  private final int currencyCode;
 
-	CashFlowCurrencyType(int currencyCode) {
-		this.currencyCode = currencyCode;
-	}
+  private CashFlowCurrencyType(int currencyCode) {
+    this.currencyCode = currencyCode;
+  }
 
-	public int getIntCurrencyCode() {
-		return currencyCode;
-	}
+  public int getIntCurrencyCode() {
+    return currencyCode;
+  }
 
-	public static final CashFlowCurrencyType getCurrencyType(int currencyCode) {
-		for (CashFlowCurrencyType current : CashFlowCurrencyType.values()) {
-			if (current.getIntCurrencyCode() != currencyCode) {
-				continue;
-			} else {
-				return current;
-			}
-		}
+  public static final CashFlowCurrencyType getCurrencyType(int currencyCode) {
+    for (CashFlowCurrencyType current : CashFlowCurrencyType.values()) {
+      if (current.getIntCurrencyCode() == currencyCode) {
+        return current;
+      }
+    }
 
-		return null;
-	}
+    return null;
+  }
 
 }
--- d-wallet-web/src/main/java/com/ddimitroff/projects/dwallet/enums/CashFlowType.java
+++ d-wallet-web/src/main/java/com/ddimitroff/projects/dwallet/enums/CashFlowType.java
@@ -1,28 +1,26 @@
 package com.ddimitroff.projects.dwallet.enums;
 
 public enum CashFlowType {
-	PROFIT(1), COST(2);
+  PROFIT(1), COST(2);
 
-	private final int type;
+  private final int type;
 
-	CashFlowType(int type) {
-		this.type = type;
-	}
+  private CashFlowType(int type) {
+    this.type = type;
+  }
 
-	public int getIntType() {
-		return type;
-	}
+  public int getIntType() {
+    return type;
+  }
 
-	public static final CashFlowType getType(int type) {
-		for (CashFlowType current : CashFlowType.values()) {
-			if (current.getIntType() != type) {
-				continue;
-			} else {
-				return current;
-			}
-		}
+  public static final CashFlowType getType(int type) {
+    for (CashFlowType current : CashFlowType.values()) {
+      if (current.getIntType() == type) {
+        return current;
+      }
+    }
 
-		return null;
-	}
+    return null;
+  }
 
 }
